7688965af28c6f886d41254d347f713fc91d572f
YarnSeemannsgarn <janschlenker1990@gmail.com>
2020-10-23 14:32:49 +0200

    Add initialization functions

--- src/DSBrowser.js
+++ src/DSBrowser.js
@@ -1,3 +1,5 @@
+import Util from './Util';
+
 const BROWSER_TYPES = {
     DS: 'DS',
     LIST: 'LIST'
@@ -9,14 +11,62 @@ class DSBrowser {
         this.dsOrList = dsOrList;
         this.type = type;
 
-        window.addEventListener('popstate', () => {
-            this.render();
+        this.util = new Util(this);
+
+        window.addEventListener('popstate', async () => {
+            await this.render();
         });
     }
 
-    render() {
+    async render() {
         this.elem.innerHTML =
             '<img src="https://raw.githubusercontent.com/YarnSeemannsgarn/ds-browser/main/images/loading.gif" '
             + 'alt="Loading Animation" style="margin-top: 6px">';
+
+        await this.init();
+    }
+
+    async init() {
+        // Init list
+        if (this.listNeedsInit()) {
+            await this.initList();
+        }
+
+        // Init vocab
+        if (this.dsNeedsInit()) {
+            await this.initDS();
+        }
+    }
+
+    listNeedsInit() {
+        return (this.type === BROWSER_TYPES.LIST && !this.list);
+    }
+
+    async initList() {
+        this.list = await this.util.parseToObject(this.dsOrList);
+    }
+
+    dsNeedsInit() {
+        const searchParams = new URLSearchParams(window.location.search);
+        const dsUID = searchParams.get('ds');
+        return ((this.type === BROWSER_TYPES.LIST && dsUID && dsUID !== this.dsUID) ||
+            (this.type === BROWSER_TYPES.DS && !this.ds));
+    }
+
+    async initDS() {
+        if (this.type === BROWSER_TYPES.DS) {
+            this.ds = await this.util.parseToObject(this.vocabOrList);
+        } else if (this.type === BROWSER_TYPES.LIST) {
+            const searchParams = new URLSearchParams(window.location.search);
+            this.dsUID = searchParams.get('ds');
+            for (const part of this.list['schema:hasPart']) {
+                const id = part['@id'];
+                if (id.split('/').pop() === this.dsUID) {
+                    this.ds = await this.util.parseToObject(id);
+                    this.dsName = part['schema:name'];
+                    break;
+                }
+            }
+        }
     }
 }
---
+++ src/Util.js
@@ -0,0 +1,60 @@
+class Util {
+    constructor(browser) {
+        this.browser = browser;
+    }
+
+    async parseToObject(variable) {
+        if (this.isString(variable)) {
+            /**
+             * @type string
+             */
+            let jsonString;
+            if (this.isValidUrl(variable)) {
+                jsonString = await this.getJson(variable);
+            } else {
+                jsonString = variable;
+            }
+            return JSON.parse(jsonString);
+        } else {
+            return variable;
+        }
+    }
+
+    isString(variable) {
+        return (typeof variable === 'string' || variable instanceof String);
+    }
+
+    isValidUrl(string) {
+        try {
+            new URL(string);
+        } catch (_) {
+            return false;
+        }
+        return true;
+    }
+
+    getJson(url) {
+        return new Promise(function (resolve, reject) {
+            let xhr = new XMLHttpRequest();
+            xhr.open('GET', url);
+            xhr.setRequestHeader('Accept', 'application/json');
+            xhr.onload = function () {
+                if (this.status >= 200 && this.status < 300) {
+                    resolve(xhr.response);
+                } else {
+                    reject({
+                        status: this.status,
+                        statusText: xhr.statusText
+                    });
+                }
+            };
+            xhr.onerror = function () {
+                reject({
+                    status: this.status,
+                    statusText: xhr.statusText
+                });
+            };
+            xhr.send();
+        });
+    }
+}
