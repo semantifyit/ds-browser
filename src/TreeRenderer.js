8b1809680f353720cde01332684082c9c828d364
YarnSeemannsgarn <janschlenker1990@gmail.com>
2020-12-07 10:42:37 +0100

    Update jQuery and jsTree CDN

--- src/TreeRenderer.js
+++ src/TreeRenderer.js
@@ -30,11 +30,13 @@ class TreeRenderer {
     createJSTreeHTML() {
         return '' +
             '<head>' +
-            '<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>' +
-            '<script src="https://cdnjs.cloudflare.com/ajax/libs/jstree/3.3.8/jstree.min.js"></script>' +
+            '<script src="https://code.jquery.com/jquery-3.5.1.min.js" ' +
+            '  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>' +
+            '<script src="https://cdnjs.cloudflare.com/ajax/libs/jstree/3.3.10/jstree.min.js"></script>' +
             '<script src="https://cdnjs.cloudflare.com/ajax/libs/jstreegrid/3.10.2/jstreegrid.min.js"></script>' +
             '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" />' +
-            '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jstree/3.3.8/themes/default/style.min.css" />' +
+            '<link rel="stylesheet" ' +
+            '  href="https://cdnjs.cloudflare.com/ajax/libs/jstree/3.3.10/themes/default/style.min.css" />' +
             this.createTreeStyle() +
             '</head>' +
             '<body>' +
